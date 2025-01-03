3125351d7141fd4d5f61af0a87e0cb8751872fe7
kmin1231 <kmin91143@gmail.com>
2025-01-03 23:02:39 +0900

    ✔️ chapter05: models updated

---
+++ planner/models/users.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!",
+                "events": [],
+            }
+        }
