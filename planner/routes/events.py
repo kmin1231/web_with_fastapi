3665f28bbd5c26a25b4772a028e8bec82c4f4165
kmin1231 <kmin91143@gmail.com>
2025-01-04 21:41:01 +0900

    ✔️ chapter05: event route added: POST & DELETE

--- planner/routes/events.py
+++ planner/routes/events.py
@@ -12,6 +12,8 @@ events = []
 async def retrieve_all_events() -> List[Event]:
     return events
 
+# $ curl localhost:8000/event/
+
 @event_router.get("/{id}", response_model=Event)
 async def retrieve_event(id: int) -> Event:
     for event in events:
@@ -21,3 +23,38 @@ async def retrieve_event(id: int) -> Event:
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist"
     )
+
+# $ curl localhost:8000/event/1
+
+@event_router.post("/new")
+async def create_event(body: Event = Body(...)) -> dict:
+    events.append(body)
+    return {
+        "message": "Event created successfully."
+    }
+
+# $ curl -X POST localhost:8000/event/new -H 'accept: application/json' -H 'Content-Type: application/json' -d '{"id": 1, "title": "FastAPI Book Launch", "image": "https://linktomyimage.com/image.png", "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!", "tags": ["Python", "fastapi", "book", "launch"], "location": "Google Meet"}'
+
+@event_router.delete("/{id}")
+async def delete_event(id: int) -> dict:
+    for event in events:
+        if event.id == id:
+            events.remove(event)
+            return {
+                "message": "Event deleted successfully."
+            }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Events with supplied ID does not exist"
+    )
+
+# $ curl -X DELETE localhost:8000/event/1
+
+@event_router.delete("/")
+async def delete_all_events() -> dict:
+    events.clear()
+    return {
+        "message": "Events deleted successfully."
+    }
+
+# $ curl -X DELETE localhost:8000/event/
