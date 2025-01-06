e8063fb5a683014ae635fef402e013393e0f3f81
kmin1231 <kmin91143@gmail.com>
2025-01-07 00:40:05 +0900

    ✔️ chapter06: (PUT) modify event data

--- planner/routes/events.py
+++ planner/routes/events.py
@@ -66,3 +66,22 @@ async def delete_all_events() -> dict:
     }
 
 # $ curl -X DELETE localhost:8000/event/
+
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(id: int, new_data: EventUpdate, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+
+        return event
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
+
+# $ curl -X PUT localhost:8000/event/edit/1 -H 'accept: application/json' -H 'Content-Type: application/json' -d '{"title": "Packts FastAPI book launch II"}'
