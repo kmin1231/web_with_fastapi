cc1c875ab18d65558761bb3c319b289edbf7a966
kmin1231 <kmin91143@gmail.com>
2025-01-13 06:42:21 +0900

    ✔️ chapter07: routing for signup updated

--- planner/routes/users.py
+++ planner/routes/users.py
@@ -2,21 +2,27 @@ from fastapi import APIRouter, HTTPException, status
 from database.connection import Database
 
 from models.users import User, UserSignIn
+from auth.hash_password import HashPassword
 
 user_router = APIRouter(
     tags=["User"],
 )
 
 user_database = Database(User)
+hash_password = HashPassword()
 
 @user_router.post("/signup")
 async def sign_new_user(user: User) -> dict:
     user_exist = await User.find_one(User.email == user.email)
+
     if user_exist:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="User with email provided exists already"
         )
+    
+    hashed_password = hash_password.create_hash(user.password)
+    user.password = hashed_password
     await user_database.save(user)
     return {
         "message": "User successfully registered!"
