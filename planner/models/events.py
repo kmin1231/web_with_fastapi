bacaef023db3ce7e1de57147440dcb456e6e6b4a
kmin1231 <kmin91143@gmail.com>
2025-01-16 23:27:41 +0900

    ✔️ chapter07: event 'creator' field added

--- planner/models/events.py
+++ planner/models/events.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class Event(Document):
+    creator: Optional[str]
     title: str
     image: str
     description: str
--- planner/routes/events.py
+++ planner/routes/events.py
@@ -35,6 +35,7 @@ async def retrieve_event(id: PydanticObjectId) -> Event:
 
 @event_router.post("/new")
 async def create_event(body: Event, user: str = Depends(authenticate)) -> dict:
+    body.creator = user
     await event_database.save(body)
 
     return {
@@ -82,6 +83,13 @@ async def delete_all_events() -> dict:
 
 @event_router.put("/edit/{id}", response_model=Event)
 async def update_event(id: PydanticObjectId, body: EventUpdate, user: str = Depends(authenticate)) -> Event:
+    event =await event_database.get(id)
+    if event.creator != user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Operation not allowed"
+        )
+    
     updated_event = await event_database.update(id, body)
     
     if not updated_event:
