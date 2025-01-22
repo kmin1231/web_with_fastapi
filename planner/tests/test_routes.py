b697f30805b652481bae14682b9053c976f65b69
kmin1231 <kmin91143@gmail.com>
2025-01-22 23:52:03 +0900

    ✔️ chapter08: test fixture for access token & mock event

---
+++ planner/tests/test_routes.py
@@ -0,0 +1,26 @@
+import httpx
+import pytest
+from typing import AsyncGenerator
+
+from planner.auth.jwt_handler import create_access_token
+from planner.models.events import Event
+
+
+@pytest.fixture(scope="module")
+async def access_token() -> str:
+    return create_access_token("testuser@packt.com")
+
+@pytest.fixture(scope="module")
+async def mock_event() -> AsyncGenerator[Event, None]:
+    new_event = Event(
+        creator="testuser@packt.com",
+        title="FastAPI Book Launch",
+        image="https://linktomyimage.com/image.png",
+        description="We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+        tags=["python", "fastapi", "book", "launch"],
+        location="Google Meet"
+    )
+
+    await Event.insert_one(new_event)
+
+    yield new_event
